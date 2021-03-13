1bdea49b875aa468bc28a70686f86b568f17b076
Justin Walsh <justinw@mortgage365.com>
2021-03-13 10:51:58 -0700

    fix: formatting

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
@@ -9,30 +9,58 @@ const NavLink = styled(Link)`
   ${"@apply text-gray-300 hover:bg-gray-700 hover:text-white"};
   &[aria-current] {
     ${"@apply bg-gray-900 text-white hover:bg-gray-900"};
-  };
-`;
+  }
+`
 
-const MobileNavButton = ({ active = false, onClick }) => {
-  return (
-    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-      {/* Mobile menu button */}
-      <button type="button" onClick={onClick} className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        {/* Icon when menu is closed. */}
-        <svg className={`${active === false ? "block" : "hidden"} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-        {/* Icon when menu is open */}
-        <svg className={`${active === true ? "block" : "hidden"} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-        </svg>
-      </button>
-    </div>
-  );
-}
+const MobileNavButton = ({ active = false, onClick }) => (
+  <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+    {/* Mobile menu button */}
+    <button
+      type="button"
+      onClick={onClick}
+      className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+      aria-controls="mobile-menu"
+      aria-expanded="false"
+    >
+      <span className="sr-only">Open main menu</span>
+      {/* Icon when menu is closed. */}
+      <svg
+        className={`${active === false ? "block" : "hidden"} h-6 w-6`}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+      {/* Icon when menu is open */}
+      <svg
+        className={`${active === true ? "block" : "hidden"} h-6 w-6`}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </button>
+  </div>
+)
 
 const MobileNavMenu = ({ active = false, siteTitle = "" }) => {
-  if (!active) return <></>;
+  if (!active) return <></>
 
   return (
     <div className="sm:hidden" id="mobile-menu">
@@ -42,20 +70,26 @@ const MobileNavMenu = ({ active = false, siteTitle = "" }) => {
         <NavLink to="/using-typescript/">Using TypeScript</NavLink>
       </div>
     </div>
-  );
+  )
 }
 
-const Header = ({ siteTitle }) => { 
-  const [menuActive, setMenuActive] = useState(false);
+const Header = ({ siteTitle }) => {
+  const [menuActive, setMenuActive] = useState(false)
   return (
     <header>
       <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex items-center justify-between h-16">
             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-              <MobileNavButton active={menuActive} onClick={() => setMenuActive(!menuActive) } />
+              <MobileNavButton active={menuActive} onClick={() => setMenuActive(!menuActive)} />
               <div className="flex-shrink-0 flex items-center">
-                <StaticImage className="block h-8 w-8" src="../images/gatsby-icon.png" width={32} height={32} alt="Gatsby Logo" />
+                <StaticImage
+                  className="block h-8 w-8"
+                  src="../images/gatsby-icon.png"
+                  width={32}
+                  height={32}
+                  alt="Gatsby Logo"
+                />
               </div>
               <div className="hidden sm:block sm:ml-6">
                 <div className="flex space-x-4">
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,50 +5,49 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
- import * as React from "react"
- import PropTypes from "prop-types"
- import { useStaticQuery, graphql } from "gatsby"
- import styled from "astroturf/react"
- 
- import Header from "./header"
- 
- const Layout = ({ children }) => {
-   const data = useStaticQuery(graphql`
-     query SiteTitleQuery {
-       site {
-         siteMetadata {
-           title
-         }
-       }
-     }
-   `)
- 
-   const Content = styled.div`
+import * as React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import styled from "astroturf/react"
+
+import Header from "./header"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  const Content = styled.div`
     ${"@apply mb-auto h-full p-10"};
-   `;
- 
-   const Footer = styled.footer`
-     ${"@apply flex h-6 justify-center text-sm text-gray-800 font-light"};
-   `;
- 
-   return (
-     <div className="flex flex-col h-screen">
-       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-       <Content>{children}</Content>
-       <Footer>
-         <p>
-           © {new Date().getFullYear()}, Built with
-           {` `}
-           <a href="https://www.gatsbyjs.com">Gatsby</a>
-         </p>
-       </Footer>
-     </div>
-   )
- }
- 
- Layout.propTypes = {
-   children: PropTypes.node.isRequired,
- }
- 
- export default Layout
- 
+  `
+
+  const Footer = styled.footer`
+    ${"@apply flex h-6 justify-center text-sm text-gray-800 font-light"};
+  `
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Content>{children}</Content>
+      <Footer>
+        <p>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </p>
+      </Footer>
+    </div>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
